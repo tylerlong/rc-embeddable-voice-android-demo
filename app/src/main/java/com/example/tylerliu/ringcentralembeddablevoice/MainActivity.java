bd93d0cab647aa4a6f91eee653d036175949ea85
Tyler Long <tyler4long@gmail.com>
2018-05-24 14:55:27 +0800

    Enable remote debugging

--- app/src/main/java/com/example/tylerliu/ringcentralembeddablevoice/MainActivity.java
+++ app/src/main/java/com/example/tylerliu/ringcentralembeddablevoice/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView.setWebContentsDebuggingEnabled(true);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("https://ringcentral.github.io/ringcentral-embeddable-voice/app.html");
     }
